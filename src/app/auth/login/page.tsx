02c5f4add4777c399e043fb6fa484961af0bf248
Leonardo Braga <programer_frontbraga@outlook.com>
2025-03-12 17:10:36 -0300

    feat: create initial login-form and login page structure

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+import { AuthSection } from '../components/auth-section';
+import { LoginForm } from '../components/login-form';
+
 export default function LoginPage() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+	return (
+		<AuthSection>
+			<div className="bg-christalle/75 mt-20 flex w-[85%] max-w-lg flex-col items-center gap-2.5 rounded-2xl p-2.5 shadow-xl md:mt-0">
+				<h2 className="font-gantari text-white">Faça o seu login</h2>
+
+				<LoginForm />
+				{/* <GoogleLoginForm /> */}
+
+				<div className="mt-3.5 flex w-full flex-col self-start text-white">
+					<p>Não possui uma conta?</p>
+					<Link
+						href="/register"
+						className="font-rajdhani underline underline-offset-4"
+					>
+						Cadastre-se agora mesmo e aproveite!
+					</Link>
+				</div>
+			</div>
+		</AuthSection>
+	);
 }

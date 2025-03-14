90278750a69400193c0a48c4d22913411f4f13ca
Leonardo Braga <programer_frontbraga@outlook.com>
2025-03-14 12:59:55 -0300

    chore: implement session rules to AuthLayout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,16 @@
+import { redirect } from 'next/navigation';
+import { auth } from '../../../auth';
 import { AuthBackground } from './components/auth-background';
 import { AuthHeader } from './components/auth-header';
 
-export default function AuthLayout({
+export default async function AuthLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
+	const session = await auth();
+	if (session) redirect('/dashboard');
+
 	return (
-		<main className="min-h-screen w-full flex-col md:flex-row flex items-center justify-center">
+		<main className="flex min-h-screen w-full flex-col items-center justify-center md:flex-row">
 			<AuthBackground />
 			<AuthHeader />
 			{children}
